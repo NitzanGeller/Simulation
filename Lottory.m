a7ec19dd020cc151f8cacefee58b7c25c92419a5
NitzanGeller <nizan.geller@gmail.com>
2016-11-03 23:30:42 +0200

    question 2 - lottory
    lottory function - returns N most common values between 1 and maxValue,
    based on a given number of repeats.

---
+++ Lottory.m
@@ -0,0 +1,26 @@
+%question 2
+function maxValues = Lottory(maxValue,N,repeats)
+%maxValue represents the roll's max possible value. assumed to be positive.
+%N represents output size - the number of distinct values.
+%repeats represents number of rolls.
+%example: Lottory(36,6,100) returns 6 most common values between 1 and 36, based on 100 random rolls.
+
+%sample will store all rolled values
+sample=[]; 
+
+for i=1:repeats
+	%adds N distinct values to sample each roll
+	sample=[sample randperm(maxValue,N)]; 
+end
+
+%counts the number of occurrences of each rolled value.
+freq = accumarray(sample(:),1)	; 
+
+% sortedFreq is in descending order. Therefore, the first N elements are the N maximum values.
+[sortedFreq,sortedIndices] = sort(x,'descend');
+
+
+maxValueIndices = sortingIndices(1:N);
+maxValues = sortedFreq(1:Values);
+
+
